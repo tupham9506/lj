c76303d5dc1edf93747a1a98791ba508bdb3ed87
tupb <tupb@sudobo.com>
2020-04-08 18:27:02 +0700

    dist generate

---
+++ web/dist/_nuxt/82d36a5f175dafb35b6f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{384:function(n,o,l){"use strict";l.r(o);var e=l(6),component=Object(e.a)({},void 0,void 0,!1,null,null,null);o.default=component.exports}}]);
